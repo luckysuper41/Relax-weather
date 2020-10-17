25a09301fb73f2e100074074b24a1401affc0bbe
NGUYEN Huynh Dung <57275058+luckysuper41@users.noreply.github.com>
2020-10-17 16:59:13 +0200

    Updates

---
+++ precache-manifest.09c8e29bd3e0bf061d6607377e3ce2ca.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1a17f267204eab392515b56ae65efba",
+    "url": "/Relax-weather/index.html"
+  },
+  {
+    "revision": "4fc501be9123caba1af2",
+    "url": "/Relax-weather/static/css/main.f846792d.chunk.css"
+  },
+  {
+    "revision": "dbaf48c363dac5e4ff1b",
+    "url": "/Relax-weather/static/js/2.a05be969.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Relax-weather/static/js/2.a05be969.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fc501be9123caba1af2",
+    "url": "/Relax-weather/static/js/main.9fb634b4.chunk.js"
+  },
+  {
+    "revision": "59868451c496c6249cd3",
+    "url": "/Relax-weather/static/js/runtime-main.99b7fe85.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/Relax-weather/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/Relax-weather/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
